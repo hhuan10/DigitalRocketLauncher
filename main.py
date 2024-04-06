ec4f98ee1b31415f25cefd1576db28c740962529
tianhu.hh <tianhu.hh@alibaba-inc.com>
2024-04-06 17:58:09 +0800

    renam

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 '''
    Author:  YiHang Huang
-   Purpose: Main entry point for the MathGame
-   MathGame is a simple math game that generates random math problems for the user to solve.
+   Purpose: Main entry point for the Digital Rocket Launcher
+   Digital Rocket Launcher is a simple math game that generates random math problems for the user to solve.
    Created: 2024.4.5
 '''
 import asyncio
-from src.math import MathGame
+from src.rocket import DigitalRocketLauncher
 
 if __name__ == "__main__":
-    asyncio.run(MathGame().start())
+    asyncio.run(DigitalRocketLauncher().start())
